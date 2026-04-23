0c686d7d0e76698a2bb1e9158900058b3e47f177
Jochen Hoenle <jochen.hoenle@bmw.de>
2026-04-23 10:13:17 +0200

    rename server package to trlc_lsp
    Renames the Python source directory from `server/` to `trlc_lsp/`
    so the language server is a proper Python package with a qualified
    import name, matching the `-m trlc_lsp` invocation used by the
    VS Code client.

--- trlc_lsp/__main__.py
+++ trlc_lsp/__main__.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 #
-# TRLC VSCode Extension
+# TRLC LSP Server
 # Copyright (C) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
 #
-# This file is part of the TRLC VSCode Extension.
+# This file is part of the TRLC LSP Server.
 #
-# The TRLC VSCode Extension is free software: you can redistribute it
+# The TRLC LSP Server is free software: you can redistribute it
 # and/or modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 #
-# The TRLC VSCode Extension is distributed in the hope that it will be
+# The TRLC LSP Server is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
@@ -25,27 +25,44 @@
 
 import argparse
 import logging
+import os
 import sys
+import tempfile
 
 # Uncomment for debugging purposes
 # import debugpy
 # debugpy.connect(5678)
 # debugpy.breakpoint()
-# logging.basicConfig(filename="pygls.log", level=logging.DEBUG, filemode="w")
-logging.basicConfig(filename="pygls.log", level=logging.WARNING, filemode="w")
+# logging.basicConfig(
+#     filename=os.path.join(tempfile.gettempdir(), "pygls.log"),
+#     level=logging.DEBUG, filemode="w")
+logging.basicConfig(
+    filename=os.path.join(tempfile.gettempdir(), "pygls.log"),
+    level=logging.WARNING,
+    filemode="w")
 
-
-if len(sys.path) <= 1:
-    sys.path.append("python-deps")
-else:
-    sys.path.insert(1, "python-deps")
+# All extension dependencies are installed into a python-deps/ directory
+# next to this package.  Locate it relative to __file__ so it works
+# regardless of the working directory.
+_python_deps = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "python-deps"
+)
+if os.path.isdir(_python_deps):
+    if len(sys.path) <= 1:
+        sys.path.append(_python_deps)
+    else:
+        sys.path.insert(1, _python_deps)
 
 from .server import trlc_server
 
 
 def add_arguments(parser):
-    parser.description = "trlc langage server"
+    parser.description = "TRLC Language Server"
 
+    parser.add_argument("--stdio",
+                        action="store_true",
+                        help="Use stdio transport (default)")
     parser.add_argument("--tcp",
                         action="store_true",
                         help="Use TCP server")
